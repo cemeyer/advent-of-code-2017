4541639da276435177deacc5fdd818b0abd7c0a6
Conrad Meyer <cse.cem@gmail.com>
2017-12-15 18:45:18 -0800

    Day 15: Implement lcg_seed reverse function in C, with GMP mpz

--- day15/day15.c
+++ day15/day15.c
@@ -2,6 +2,11 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <gmp.h>
+
+/* Build with: cc -Wall -O3 -Wextra day15.c -g -lgmp -o day15 */
 
 unsigned
 mod_mersenne(uint64_t n, unsigned bits, unsigned mers)
@@ -57,6 +62,32 @@ countsimilar(unsigned a, unsigned b, unsigned limit, unsigned amask, unsigned bm
 	return (count);
 }
 
+unsigned
+lcg_seed(unsigned output_, unsigned idx, unsigned multiplier_, unsigned modulus_)
+{
+	unsigned res_;
+	mpz_t output, multiplier, modulus, res, mult;
+
+	mpz_init(res);
+	mpz_init(mult);
+	mpz_init_set_ui(output, output_);
+	mpz_init_set_ui(multiplier, multiplier_);
+	mpz_init_set_ui(modulus, modulus_);
+
+	/* mult = multiplier^idx */
+	mpz_powm_ui(mult, multiplier, idx, modulus);
+
+	/* res = mult^-1 * output mod modulus */
+	if (!mpz_invert(res, mult, modulus))
+		abort();
+	mpz_mul(res, res, output);
+	mpz_mod(res, res, modulus);
+
+	res_ = mpz_get_ui(res);
+	mpz_clears(res, mult, output, multiplier, modulus, NULL);
+	return (res_);
+}
+
 int
 main(int argc, char **argv)
 {
@@ -64,5 +95,7 @@ main(int argc, char **argv)
 
 	printf("Part 1 %u\n", countsimilar(A, B, 40*1000*1000, 0, 0));
 	printf("Part 2 %u\n", countsimilar(A, B, 5*1000*1000, 3, 7));
+
+	printf("LCG seed of A: %u\n", lcg_seed(next(A, 16807, 0), 1, 16807, 2147483647));
 	return (0);
 }
